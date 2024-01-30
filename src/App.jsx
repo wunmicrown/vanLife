76f1f500fcefd85d8aa85580de21393141b5bc88
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-30 21:51:09 +0100

    comitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Component/Layout'
 import LandingPage from './Component/LandingPage'
+import Footer from './Component/Footer'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<LandingPage />} />
+          {/* <Route path='/' element={<Footer/>} /> */}
         </Route>
       </Routes>
     </>
--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <>
       <div className="landing" style={{backgroundImage:`url(${landing})`,  backgroundSize:"cover", backgroundPosition:"center"}}>
-        <div className="text-center pt-36 mx-auto">
+        <div className="text-center pt-48 mx-auto">
             <h1 className="vanContent text-xl md:text-4xl lg:text-5xl sm:text-1xl">You got the travel plans, we got the travel vans.</h1>
             <p className="vanDetails pt-10 ">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <button className="text-white mt-28 bg-[#FF8C38] flex-shrink-0 w-[494.333px] h-[ 49.832px] p-4">
